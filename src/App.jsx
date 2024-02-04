edafc3d4d01e1373222b410e9e36d2fa080da68b
mjombanorman <mjomban@gmail.com>
2024-02-04 05:39:41 +0300

    Setting up card component and using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       monsters: [],
     };
   }
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
-  state = {};
+ 
   render() {
     return (
-      <div className="App">
+      <div>
+        <CardList>
         {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
+          <h4 key={monster.id}>{monster.name}</h4>
+          
         ))}
+          </CardList>
       </div>
     );
   }
