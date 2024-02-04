671c4b4eac62ccd85848f128168d2fb1a690044f
mjombanorman <mjomban@gmail.com>
2024-02-04 09:54:43 +0300

    Completed the search/filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,11 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+  
     return (
       <div>
         <input
@@ -25,7 +30,7 @@ class App extends Component {
             this.setState({ searchField: e.target.value })
           }
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
