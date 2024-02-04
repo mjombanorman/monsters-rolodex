12154255ee7ff88fea68adb1a1da2c25d5f3d7b6
mjombanorman <mjomban@gmail.com>
2024-02-04 05:14:10 +0300

    Intital setup of react project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
